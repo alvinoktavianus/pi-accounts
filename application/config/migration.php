a9b1227942bf98df3b2ef573b5d6c963b016802d
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-09 23:22:28 +0700

    Create server validation to do migration script

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20170709215450;
+$config['migration_version'] = 20170709225715;
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -5,10 +5,29 @@ class Migrate extends CI_Controller {
 
     public function index()
     {
-        if (!$this->migration->current()) {
-            show_error($this->migration->error_string());
+        $this->load->view('migration');
+
+    }
+
+    public function confirm()
+    {
+        $this->db->trans_start();
+        $where = array(
+            'email' => $this->input->post('email')
+        );
+        $result = $this->db->get_where('users', $where)->result();
+        $this->db->trans_complete();
+        if (count($result) > 0 &&
+            password_verify($this->input->post('password'), $result[0]->password) &&
+            password_verify($this->input->post('authcode'), $this->input->server('ROOT_AUTH')) &&
+            password_verify($result[0]->role, $this->input->server('ROOT_ROLE'))) {
+            if (!$this->migration->current()) {
+                show_error($this->migration->error_string());
+            } else {
+                echo 'Migrations ran successfully!';
+            }
         } else {
-            echo 'Migrations ran successfully!';
+            echo "You are not authorize to do this operation!";
         }
     }
 
