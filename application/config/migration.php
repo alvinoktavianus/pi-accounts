9d8cfef1f9e6aa3f6209f3a9f69c2d288384e6e0
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-15 01:37:31 +0700

    Create galleries table and migration script

--- application/config/migration.php
+++ application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20170712004328;
+$config['migration_version'] = 20170715012550;
 
 /*
 |--------------------------------------------------------------------------
