35faba2740821985c74ecd246841085912a2e5cf
Anthony Michael <anthony9a2@gmail.com>
2017-07-15 23:58:36 +0700

    Update Login CSS (#13)

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -4,22 +4,30 @@
     </div>
 <?php endif; ?>
 
-<?php echo form_open(base_url('login/do_login'), array('name' => 'loginForm', 'novalidate' => 'true')); ?>
+<div class="logindiv">
+    <?php echo form_open(base_url('login/do_login'), array('name' => 'loginForm', 'novalidate' => 'true', 'class' => 'loginformcss')); ?>
 
-    <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine, 'has-success': loginForm.email.$valid }">
-        <?php echo form_label('Email', 'email', array('class' => 'sr-only')); ?>
-        <?php echo form_input(array('type' => 'email', 'class' => 'form-control', 'id' => 'email', 'name' => 'email', 'ng-required' => true, 'ng-model' => 'login.email', 'placeholder' => 'Email', 'ng-pattern' => '/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/')); ?>
-        <span ng-show="!loginForm.email.$pristine && (loginForm.email.$error.required || loginForm.email.$error.pattern)" class="help-block" ng-cloak>Please enter your email address</span>
+    <div class="group">
+        <div class="form-group" ng-class="{ 'has-error' : loginForm.email.$invalid && !loginForm.email.$pristine, 'has-success': loginForm.email.$valid }">
+            <?php echo form_input(array('type' => 'email', 'id' => 'email', 'name' => 'email', 'ng-required' => true, 'ng-model' => 'login.email', 'ng-pattern' => '/^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/')); ?>
+            <span class="highlight"></span><span class="bar"></span>
+            <label>Email</label>
+            <span ng-show="!loginForm.email.$pristine && (loginForm.email.$error.required || loginForm.email.$error.pattern)" class="help-block" ng-cloak>Please enter your email address</span>
+        </div>
+    </div>
+    
+    <div class="group">
+        <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine, 'has-success': loginForm.password.$valid }">
+            <?php echo form_password('password', '', array('name' => 'password', 'ng-model' => 'login.password', 'ng-required' => true, 'ng-minlength' => 6)); ?>
+            <span class="highlight"></span><span class="bar"></span>
+           <label>Password</label>
+            <span ng-show="!loginForm.password.$pristine && (loginForm.password.$error.required || loginForm.password.$error.minlength)" class="help-block" ng-cloak>Please enter your password</span>
+        </div>
     </div>
 
-    <div class="form-group" ng-class="{ 'has-error' : loginForm.password.$invalid && !loginForm.password.$pristine, 'has-success': loginForm.password.$valid }">
-        <?php echo form_label('Password', 'password', array('class' => 'sr-only')); ?>
-        <?php echo form_password('password', '', array('class' => 'form-control', 'name' => 'password', 'ng-model' => 'login.password', 'ng-required' => true, 'placeholder' => 'Password', 'ng-minlength' => 6)); ?>
-        <span ng-show="!loginForm.password.$pristine && (loginForm.password.$error.required || loginForm.password.$error.minlength)" class="help-block" ng-cloak>Please enter your password</span>
-    </div>
-
-    <div class="form-group">
-        <button type="submit" class="btn btn-default" ng-disabled="loginForm.$invalid" ng-cloak>Log In</button>
-    </div>
+    <button type="submit" class="button buttonBlue" ng-disabled="loginForm.$invalid" ng-cloak>LOGIN
+        <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+    </button>
+</div>
 
 <?php echo form_close(); ?>
