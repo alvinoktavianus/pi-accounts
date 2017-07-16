fbe5bd64252066c322bb9ee7754678170f405300
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-17 00:44:57 +0700

    Create function to handle upload image

--- application/controllers/Galleries.php
+++ application/controllers/Galleries.php
@@ -3,12 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Galleries extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('category');
+    }
+
     public function index()
     {
         switch ($this->session->userdata('user_session')['role']) {
             case 'admin':
                 $viewData = array(
-                    'message' => 'Hello admin galleries'
+                    'message' => 'Hello admin galleries',
+                    'categories' => $this->category->create_category_dropdown()
                 );
                 break;
             case 'user':
@@ -28,6 +35,36 @@ class Galleries extends CI_Controller {
             'viewData' => $viewData
         );
         $this->load->view('main', $data);
+    }
+
+    public function do_add()
+    {
+        if ($this->session->userdata('user_session')['role'] == 'admin') {
+            $this->form_validation->set_rules('name', 'Name', 'trim|required|min_length[5]|max_length[20]');
+            $this->form_validation->set_rules('baseprice', 'Base Price', 'trim|required|integer');
+            $this->form_validation->set_rules('sellprice', 'Sell Price', 'trim|required|greater_than[baseprice]|integer');
+            $this->form_validation->set_rules('categories', 'Categories', 'required');
+
+            if (!$this->form_validation->run()) {
+                $errors = validation_errors();
+                $this->session->set_flashdata('errors', $errors);
+            } else {
+                if ($_FILES['image']['name'] != "") {
+                    $config['upload_path'] = './uploads/galleries/';
+                    $config['allowed_types'] = 'gif|jpg|png';
+                    $config['encrypt_name'] = true;
+                    $this->upload->initialize($config);
+                    if ($this->upload->do_upload('image')) {
+                        $data = $this->upload->data();
+                    } else {
+                        $this->session->set_flashdata('errors', $this->upload->display_errors());
+                    }
+                }
+            }
+            redirect('galleries','refresh');
+        } else {
+            redirect('home','refresh');
+        }
     }
 
 }
--- application/views/gallery/GalleryAdmin.php
+++ application/views/gallery/GalleryAdmin.php
@@ -1,1 +1,51 @@
-<?php echo $message; ?>
+<?php if ($this->session->flashdata('errors')): ?>
+    <div class="alert alert-danger" role="alert">
+        <strong><?php echo $this->session->flashdata('errors'); ?></strong>
+    </div>
+<?php endif; ?>
+
+<?php echo form_open_multipart(base_url('galleries/do_add'), array('name' => 'galleryForm', 'novalidate' => 'true')); ?>
+
+<fieldset>
+    <legend>Gallery Data</legend>
+
+    <div class="form-group" ng-class="{ 'has-error' : galleryForm.name.$invalid && !galleryForm.name.$pristine, 'has-success': galleryForm.name.$valid }">
+        <?php echo form_label('Name', 'name', array('class' => 'sr-only')); ?>
+        <?php echo form_input('name', '', array('class' => 'form-control', 'ng-required' => true, 'ng-model' => 'gallery.name', 'placeholder' => 'Name')); ?>
+        <span ng-show="!galleryForm.name.$pristine && galleryForm.name.$error.required" class="help-block" ng-cloak>Please enter valid product name</span>
+    </div>
+
+    <div class="form-group" ng-class="{ 'has-error' : galleryForm.baseprice.$invalid && !galleryForm.baseprice.$pristine, 'has-success': galleryForm.baseprice.$valid }">
+        <?php echo form_label('Base Price', 'baseprice', array('class' => 'sr-only')); ?>
+        <input type="text" class="form-control" placeholder="Base Price" ng-model="gallery.basePrice" name="baseprice" awnum num-int="11" num-fract="0" num-neg="false" num-sep="," num-thousand="true" num-thousand-sep="{{' '}}" ng-required="true">
+        <span ng-show="!galleryForm.baseprice.$pristine && galleryForm.baseprice.$error.required" class="help-block" ng-cloak>Please enter valid base price</span>
+    </div>
+
+    <div class="form-group" ng-class="{ 'has-error' : galleryForm.sellprice.$invalid && !galleryForm.sellprice.$pristine, 'has-success': galleryForm.sellprice.$valid }">
+        <?php echo form_label('Sell Price', 'sellprice', array('class' => 'sr-only')); ?>
+        <input type="text" awnum ng-model="gallery.sellPrice" ng-required="true" class="form-control" placeholder="Sell Price" num-fract="0" num-thousand="true" num-thousand-sep="{{' '}}" num-int="9" name="sellprice" />
+        <span ng-show="!galleryForm.sellprice.$pristine && galleryForm.sellprice.$error.required" class="help-block" ng-cloak>Please enter valid base price</span>
+    </div>
+
+    <div class="form-group" ng-class="{ 'has-error' : galleryForm.categories.$invalid && !galleryForm.categories.$pristine, 'has-success': galleryForm.categories.$valid }">
+        <?php echo form_label('Categories', 'categories', array('class' => 'sr-only')); ?>
+        <?php echo form_dropdown('categories', $categories, '0', array('class' => 'form-control', 'ng-required' => "true", "name" => "categories", 'ng-model' => 'gallery.category')); ?>
+        <span ng-show="!galleryForm.categories.$pristine && galleryForm.categories.$error.required" class="help-block" ng-cloak>Please enter valid base price</span>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend>Images</legend>
+
+    <div class="form-group">
+        <?php echo form_label('Image', 'image', array('class' => 'sr-only')); ?>
+        <input type="file" name="image" class="form-control">
+    </div>
+
+</fieldset>
+
+<div class="form-group">
+    <button ng-disabled="galleryForm.$invalid" class="btn" ng-class="{'btn-success': galleryForm.$valid, 'btn-danger': galleryForm.$invalid}" ng-cloack>Add</button>
+</div>
+
+<?php echo form_close(); ?>
