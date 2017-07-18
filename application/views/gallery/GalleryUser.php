3e40cfed58e8f55b63b27d983eaa3faa80e33557
Anthony Michael <anthony9a2@gmail.com>
2017-07-19 00:49:01 +0700

    quick CSS

--- application/views/gallery/GalleryUser.php
+++ application/views/gallery/GalleryUser.php
@@ -3,11 +3,11 @@
 if ($this->session->userdata('user_session') && $this->session->userdata('user_session')['role'] == 'user') { ?>
 	<div>
     <?php foreach ($galleries as $key => $gallery) { ?>
-    	<div class="col-md-3">
-    		<?php echo '<h4>' . $gallery->gallery_name . '</h4>'; ?>
+    	<div class="col-md-3 col-xs-6">
+    		<?php echo '<h4><strong>' . $gallery->gallery_name . '</strong></h4>'; ?>
     		<img src="<?php echo $gallery->file_url; ?>" class="imggal-satuan img-responsive img-thumbnail">
-    		<?php echo '<h5>Modal Beli : Rp ' . $gallery->base_price . '</h5>'; ?>
-    		<?php echo '<h5>Peluang Jual : Rp' . $gallery->sell_price . '</h5>'; ?>
+    		<?php echo '<h5><strong>Modal Beli : Rp ' . $gallery->base_price . '</strong></h5>'; ?>
+    		<?php echo '<h5><strong>Peluang Jual : Rp' . $gallery->sell_price . '</strong></h5>'; ?>
     	</div>
     <?php } ?>
 	</div>
