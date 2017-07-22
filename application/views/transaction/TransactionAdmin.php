e46b2ac347e3d5b1d6608dd8e3f6318545096757
Alvin Oktavianus <agentsam1995@gmail.com>
2017-07-23 00:37:23 +0700

    Create transaction user interface

--- application/views/transaction/TransactionAdmin.php
+++ application/views/transaction/TransactionAdmin.php
@@ -1,21 +1,72 @@
-<?php
-
-echo $varnota;
-
-?>
-
 <div ng-controller="TransactionCtrl">
-    <form name="transForm" novalidate="true">
+    <form name="transForm" novalidate="true" ng-submit="submitForm()">
 
         <div class="form-group">
-          <ui-select ng-model="ctrl.person.selected" theme="selectize" title="Choose a person">
-            <ui-select-match placeholder="Select or search a person in the list...">{{$select.selected.name}}</ui-select-match>
-            <ui-select-choices repeat="item in ctrl.people | filter: $select.search">
-              <div ng-bind-html="item.name | highlight: $select.search"></div>
-              <small ng-bind-html="item.email | highlight: $select.search"></small>
+          <ui-select ng-model="transaction.selected" theme="bootstrap">
+            <ui-select-match placeholder="Select or search a person in the list...">{{$select.selected.email}}</ui-select-match>
+            <ui-select-choices repeat="item in users | filter: $select.search">
+              <div ng-bind-html="item.email | highlight: $select.search"></div>
+              <small ng-bind-html="item.first_name | highlight: $select.search"></small>
+              <small ng-bind-html="item.last_name | highlight: $select.search"></small>
             </ui-select-choices>
           </ui-select>
         </div>
 
+        <div class="form-group">
+            <label class="sr-only">Full Name</label>
+            <input type="text" readonly="true" class="form-control" value="{{transaction.selected.first_name + ' ' + transaction.selected.last_name}}" placeholder="Full Name">
+        </div>
+
+        <div class="form-group">
+            <label class="sr-only">Address</label>
+            <textarea class="form-control" style="resize: vertical;" readonly="true">{{transaction.selected.address}}</textarea>
+        </div>
+
+        
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Item Name</th>
+                        <th>Qty</th>
+                        <th>Price</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr ng-repeat="item in transaction.items track by $index">
+                        <td>{{$index+1}}</td>
+                        <td>
+                            <input type="text" ng-model="item.name" class="form-control">
+                        </td>
+                        <td>
+                            <input type="number" ng-model="item.qty" class="form-control" ng-change="calcTotalPrice()">
+                        </td>
+                        <td>
+                            <input type="number" ng-model="item.price" class="form-control" ng-change="calcTotalPrice()">
+                        </td>
+                        <td>
+                            <button ng-click="addItem(item)" class="btn btn-success">Add Item</button>
+                            <button ng-click="removeItem(item)" ng-show="$index > 0" class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">Total Price</td>
+                        <td><strong>{{transaction.total_price | number: 0}}</strong></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Save" class="btn btn-primary">
+        </div>
+
     </form>
+
+    <?php if ($this->input->server('CI_ENV') == 'development') : ?>
+        <pre><code>{{transaction | json}}</code></pre>
+    <?php endif; ?>
 </div>
--- src/js/TransactionController.js
+++ src/js/TransactionController.js
@@ -2,17 +2,35 @@ app.controller('TransactionCtrl', ['$scope', '$http', '$q', function($scope, $ht
 
     $scope.users = [];
     $scope.transaction = {};
+    $scope.transaction.items = [{}];
+    $scope.transaction.total_price = 0;
 
     function init() {
         // get all users
         $http.get(baseUrl+'/users/get_all_users', {}).then(function(response) {
-            // console.log(response);
             $scope.users = response.data;
-            $scope.transaction.user = $scope.users[0];
         }, function() {
 
         });
     }
+
+    $scope.addItem = function() {
+        $scope.transaction.items.push({});
+    };
+
+    $scope.removeItem = function(item) {
+        var index = $scope.transaction.items.indexOf(item);
+        $scope.transaction.items.splice(index, 1); 
+    };
+
+    $scope.calcTotalPrice = function() {
+        $scope.transaction.total_price = 0;
+        angular.forEach($scope.transaction.items, function(item) { $scope.transaction.total_price += !isNaN(item.qty * item.price) ? (item.qty * item.price) : 0; });
+    };
+
+    $scope.submitForm = function() {
+        console.log("form submitted");
+    };
 
     angular.element(document).ready(function () {
         init();
