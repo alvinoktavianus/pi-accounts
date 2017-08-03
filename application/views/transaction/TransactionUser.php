06481d17b6ca5daf37c12cbfe3b7a1a794f4a29a
Alvin Oktavianus <agentsam1995@gmail.com>
2017-08-03 14:32:00 +0700

    Improve date and number formatter

--- application/views/transaction/TransactionUser.php
+++ application/views/transaction/TransactionUser.php
@@ -1,7 +1,7 @@
 <h2>
 <?php
 	$date = new DateTime($transaction['created_at']);
-	echo $transaction['invoice_no'] . ' - ' . $date->format('D, d M Y'); ?>
+	echo $transaction['invoice_no'] . ' - ' . $date->format('d F Y | H:i'); ?>
 		
 </h2>
 
@@ -18,19 +18,19 @@
 		<td class="text-center"><?php echo $key+1; ?></td>
 		<td><?php echo $value['item_name']; ?></td>
 		<td class="text-center"><?php echo $value['quantity']; ?></td>
-		<td class="text-center"><?php echo 'Rp. ' . number_format($value['price'], 2); ?></td>
-		<td class="text-center"><?php echo 'Rp. ' . number_format($value['quantity'] * $value['price'], 2); ?></td>
+		<td class="text-center"><?php echo 'Rp ' . number_format($value['price'], 0, ",", "."); ?></td>
+		<td class="text-center"><?php echo 'Rp ' . number_format($value['quantity'] * $value['price'], 0, ",", "."); ?></td>
 	</tr>
 <?php } ?>
 	<tr>
 		<td colspan="3"></td>
 		<td class="text-center"><strong>ONGKOS KIRIM</strong></td>
-		<td class="text-center"><?php echo 'Rp. ' . number_format($transaction['shipping_fee'], 2); ?></td>
+		<td class="text-center"><?php echo 'Rp ' . number_format($transaction['shipping_fee'], 0, ",", "."); ?></td>
 	</tr>
 	<tr>
 		<td colspan="3"></td>
 		<td class="text-center"><strong>GRAND TOTAL</strong></td>
-		<td class="text-center"><?php echo 'Rp. ' . number_format($transaction['total'], 2); ?></td>
+		<td class="text-center"><?php echo 'Rp ' . number_format($transaction['total'], 0, ",", "."); ?></td>
 	</tr>
 
 </table>
